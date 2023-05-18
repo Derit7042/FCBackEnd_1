1a16205a533bd211e99a6922525ea65328ae6bc3
Derit7042 <junsu6752@gmail.com>
2023-05-19 02:41:00 +0900

    2023_05_18_2

--- src/MovieTest.java
+++ src/MovieTest.java
@@ -1,13 +1,13 @@
 public class MovieTest {
     public static void main(String[] args) {
-        Movie m = new Movie();
-        m.title = "아바타";
-        m.day = "2202.12.14";
-        m.cha = "제이크 설리";
-        m.jan = "액션";
-        m.time = 192;
-        m.age = "12세";
-        System.out.println(m.title + "\t" + m.day + "\t" + m.cha + "\t" + m.jan + "\t" + m.time + "\t" + m.age);
+        Movie mv = new Movie();
+        mv.title = "아바타";
+        mv.day = "2202.12.14";
+        mv.cha = "제이크 설리";
+        mv.jan = "액션";
+        mv.time = 192;
+        mv.age = "12세";
+        System.out.println(mv.title + "\t" + mv.day + "\t" + mv.cha + "\t" + mv.jan + "\t" + mv.time + "\t" + mv.age);
 
 
     }

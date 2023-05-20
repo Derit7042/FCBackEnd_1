e7367e6a0dfc7c93d3610a84c5700844b8c76dc1
Derit7042 <junsu6752@gmail.com>
2023-05-21 00:33:41 +0900

    2023_05_20_3

---
+++ src/ArrayExam.java
@@ -0,0 +1,11 @@
+public class ArrayExam {
+    public static void main(String[] args) {
+        // Q. 아래 char[] 배열에 APPLE라는 문자가 저장되어 있다. 대문잘 된 APPLE을 소문자 apple로 출력하세요.
+        char[] c = {'A', 'P', 'P', 'L', 'E'};
+        int a = c[0] - 32;
+        int p = c[1] - 32;
+        int l = c[3] - 32;
+        int e = c[4] - 32;
+
+    }
+}

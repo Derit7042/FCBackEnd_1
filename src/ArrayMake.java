5a94e7e4453a52cb9e97135626fe374e2210b889
Derit7042 <junsu6752@gmail.com>
2023-05-20 19:44:02 +0900

    2023_05_20_1

---
+++ src/ArrayMake.java
@@ -0,0 +1,10 @@
+public class ArrayMake {
+    public static void main(String[] args) {
+        // Q. [정수 5개]를 저장 할 [배열을 생성]하시오.
+        int[] a = new int[5];
+
+        float[] f = new float[5]; // 길이가 고정된다. 한 번 5개로 만들면 무조건 5개로 고정
+
+        Book[] b = new Book[3]; // 책 3권을 저장할 배열을 생성
+    }
+}

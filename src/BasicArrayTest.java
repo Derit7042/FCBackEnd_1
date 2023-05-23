1b64b829555a17c721a5c4b31ccb6958eab61d80
Derit7042 <junsu6752@gmail.com>
2023-05-23 23:32:40 +0900

    2023_05_23_3

--- src/BasicArrayTest.java
+++ src/BasicArrayTest.java
@@ -15,7 +15,7 @@ public class BasicArrayTest {
         b[0].author = "박매일";
         b[0].page = 500;
         b[0].isbn = "11889900";
-        System.out.println(b[0].title + "\t" + b[0].price + "\t" + b[0].company + "\t" + b[0].author + "\t" + b[0].page + "\t" + b[0].isbn);
+        //System.out.println(b[0].title + "\t" + b[0].price + "\t" + b[0].company + "\t" + b[0].author + "\t" + b[0].page + "\t" + b[0].isbn);
 
         b[1] = new Book();
         b[1].title = "오라클";
@@ -24,7 +24,7 @@ public class BasicArrayTest {
         b[1].author = "박매일";
         b[1].page = 500;
         b[1].isbn = "11889900";
-        System.out.println(b[1].title + "\t" + b[1].price + "\t" + b[1].company + "\t" + b[1].author + "\t" + b[1].page + "\t" + b[1].isbn);
+        //System.out.println(b[1].title + "\t" + b[1].price + "\t" + b[1].company + "\t" + b[1].author + "\t" + b[1].page + "\t" + b[1].isbn);
 
         b[2] = new Book();
         b[2].title = "인공지능";
@@ -33,6 +33,10 @@ public class BasicArrayTest {
         b[2].author = "박매일";
         b[2].page = 500;
         b[2].isbn = "11889900";
-        System.out.println(b[2].title + "\t" + b[2].price + "\t" + b[2].company + "\t" + b[2].author + "\t" + b[2].page + "\t" + b[2].isbn);
+        //System.out.println(b[2].title + "\t" + b[2].price + "\t" + b[2].company + "\t" + b[2].author + "\t" + b[2].page + "\t" + b[2].isbn);
+
+        for (int i = 0; i < b.length; i++) {
+            System.out.println(b[i].title + "\t" + b[i].price + "\t" + b[i].company + "\t" + b[i].author + "\t" + b[i].page + "\t" + b[i].isbn);
+        }
     }
 }

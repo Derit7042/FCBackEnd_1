534f67b5c22d6564dcc1ab7f1f581f9edaa5c116
Derit7042 <junsu6752@gmail.com>
2023-05-28 21:12:18 +0900

    2023_05_28_2

--- src/ObjectArrayExam.java
+++ src/ObjectArrayExam.java
@@ -9,10 +9,26 @@ public class ObjectArrayExam {
         m[0].age = 15;
         m[0].day = "2023-01-15";
 
-
-
         m[1] = new Movie();
+        m[1] = new Movie();
+        m[1].title = "B영화";
+        m[1].cha = "나길동";
+        m[1].time = 180;
+        m[1].age = 12;
+        m[1].day = "2023-01-11";
+
         m[2] = new Movie();
+        m[2] = new Movie();
+        m[2].title = "C영화";
+        m[2].cha = "조길동";
+        m[2].time = 140;
+        m[2].age = 18;
+        m[2].day = "2023-11-16";
+
+        // 반복문
+        for(int i = 0; i < m.length; i++){
+            System.out.println(m[i].title + "\t" + m[i].cha + "\t" + m[i].time + "\t" + m[i].age + "\t" + m[i].day);
+        }
 
     }
 }
